06c1c1d3e3bb687cd68158f301859a584ba59f95
Madeline Jones <brandkast@live.com>
2019-01-17 17:13:36 -0500

    Updates

---
+++ precache-manifest.7e144760fadc294209382a7ae80ea0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e02486006c5e23adb35",
+    "url": "/pokedex/static/css/main.35cce2e4.chunk.css"
+  },
+  {
+    "revision": "6e02486006c5e23adb35",
+    "url": "/pokedex/static/js/main.6e024860.chunk.js"
+  },
+  {
+    "revision": "e3b3b06d3d743d14d940",
+    "url": "/pokedex/static/css/1.db6ef75d.chunk.css"
+  },
+  {
+    "revision": "e3b3b06d3d743d14d940",
+    "url": "/pokedex/static/js/1.e3b3b06d.chunk.js"
+  },
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "27ff7df05554e358e19903c84066ebc2",
+    "url": "/pokedex/index.html"
+  }
+];
